ccc8ba17b309bf930cbc72d3706e63b6b4a99af2
Nikesh <nikesh.tadela@gmail.com>
2025-02-20 19:42:02 +0530

    Modified example code to Map only program with little modifications

---
+++ MapOnly.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.util.Random;
+
+public class MapOnly {
+
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, Text>{
+
+    private Text line = new Text();
+    private Text docId = new Text();
+    Random r = new Random();
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      String id = "docId=" + r.nextInt(530);
+      docId.set(id);
+      context.write(value, docId);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(MapOnly.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
